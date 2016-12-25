97f769303f3a683d9b73df6195d73bb480221a1e
Evgeny Ptitsyn <eptitsyn@gmail.com>
2016-12-25 16:42:00 +0300

    b4 change

--- New folder/task10/task10/Program.cs
+++ New folder/task10/task10/Program.cs
@@ -1,41 +1,144 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace task10
 {
     class Program
     {
-        class Person
+
+        [Serializable] public class Person
         {
-            string Firstname;
-            string Lastname;
-            string Patronymic;
-            string Phone_number;
-            string Address;
+            public string Firstname { get; set; }
+            public string Lastname { get; set; }
+            public string Patronymic { get; set; }
+            public string Phone_number { get; set; }
+            public string Address { get; set; }
+        }
+
+        [Serializable]
+        public class Persons
+        {
+            List<Person> Base { get; set; }
+            Persons() { Base = new List<Person>();}
+           Persons(List<Person> a) { Base = a;}
+        }
+
+        private static void SavePersons(Persons Base, string filename)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Persons));            
+            using (FileStream stream = new FileStream(filename, FileMode.Create)) 
+            {
+                serializer.Serialize(stream, Base);
+            }
+        }
+
+        private static Persons LoadPersons(string filename)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Person>));
+            using (FileStream stream = new FileStream(filename, FileMode.Open))
+            {
+               // if (serializer.CanDeserialize(stream))
+                {
+                    return new Persons((List<Person>)serializer.Deserialize(stream));;
+                }
+                //else
+                //{
+                //    throw new Exception();
+                //}
+
+            }
+        }
+
+        private static List<Person> dosearch(List<Person> Base, string Fieldname, string searchvalue)
+        {
+            FieldInfo fi = typeof(Person).GetField(Fieldname);
+            return Base.FindAll(person => fi.GetValue(person).ToString().Contains(searchvalue));
+        }
+
+        private static void AddPerson(ref List<Person> Base)
+        {
+            Person person = new Person();
+            Console.Write("Add Person. Name:");
+            person.Firstname = Console.ReadLine();
+            Console.Write("Add Person. Lastame:");
+            person.Lastname = Console.ReadLine();
+            Console.Write("Add Person. Patronymic:");
+            person.Patronymic = Console.ReadLine();
+            Console.Write("Add Person. Phone:");
+            person.Phone_number = Console.ReadLine();
+            Console.Write("Add Person. Address:");
+            person.Address = Console.ReadLine();
+
+            Base.Add(person);
+        }
+
+        private static void SearchPerson(ref List<Person> Base)
+        {
+            
+            int choose;
+            string fieldname = "", value;
+            while ((choose = Console.ReadKey().KeyChar) >= '1' && choose <= 5 )
+            {
+                Console.WriteLine("Searchperson by:");
+                Console.WriteLine("1 - Firstname");
+                Console.WriteLine("2 - Lastname");
+                Console.WriteLine("3 - Patronymic");
+                Console.WriteLine("4 - Phone");
+                Console.WriteLine("5 - Address");
+                switch (choose)
+                {
+                    case '1': { fieldname = "Firstname"; break; }
+                    case '2': { fieldname = "Lastname"; break; }
+                    case '3': { fieldname = "Patronymic"; break; }
+                    case '4': { fieldname = "Phone_number"; break; }
+                    case '5': { fieldname = "Address"; break; }
+                }
+            }
+            Console.Write("Value:");
+            value = Console.ReadLine();
+            List<Person> result = dosearch(Base, fieldname, value);
+            Console.WriteLine("Result");
+            foreach (Person person in result)
+            {
+                Console.WriteLine(person.ToString());
+            }
         }
         static void Main(string[] args)
         {
+            Persons Base;
             //LoadBase
-            List<Person> Base;
-        }
-
-        private List<Person> search(List<Person> Base, string Fieldname, string searchvalue)
-        {
-            List<Person> resultList = new List<Person>();
-            foreach (Person recordPerson in Base)
+            try
             {
-                FieldInfo fi = typeof (Person).GetField(Fieldname);
-                if (fi.GetValue(recordPerson).ToString().Contains(searchvalue))
+                Base = LoadPersons("perxons.xml");
+            }
+            catch (Exception)
+            {
+                Base = new Persons();
+            }
+
+            int c;
+            while ((c = Console.ReadKey().KeyChar) != '0')
+            {
+                Console.Clear();
+                Console.WriteLine("1 - add person");
+                Console.WriteLine("2 - search person");
+                Console.WriteLine("0 - quit");
+                switch (c)
                 {
-                    resultList.Add(recordPerson);
+                    case '1': { AddPerson(ref Base); break;}
+                    case '2': { SearchPerson(ref Base); break; }
                 }
             }
-            return resultList;
+            SavePersons(Base, "persons.xml");
         }
+
+
     }
 }
